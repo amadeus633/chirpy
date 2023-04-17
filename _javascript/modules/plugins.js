f9e003bc186d3d7f94e0e6c7812feddb4a3340a9
amadeus633 <60037780+amadeus633@users.noreply.github.com>
2023-04-17 00:25:05 -0400

    Update plugins.js

--- _javascript/modules/plugins.js
+++ _javascript/modules/plugins.js
@@ -5,3 +5,4 @@ export { initLocaleDatetime } from './components/locale-datetime';
 export { initPageviews } from './components/pageviews';
 export { smoothScroll } from './components/smooth-scroll';
 export { toc } from './components/toc';
+export { function } from './components/cursor';
